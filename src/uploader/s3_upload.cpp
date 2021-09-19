c63654424021aec75899a3c8831dd059b37244fe
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-09-19 23:49:12 +0200

    simplify some s3 upload things

--- src/uploader/s3_upload.cpp
+++ src/uploader/s3_upload.cpp
@@ -8,31 +8,20 @@ void s3_upload(S3Mirror& mirror,
     std::string digest = sha256sum(file);
     std::size_t fsize = fs::file_size(file);
 
-
-    // PUT /my-image.jpg HTTP/1.1
-    // Host: myBucket.s3.<Region>.amazonaws.com
-    // Date: Wed, 12 Oct 2009 17:50:00 GMT
-    // Authorization: authorization string
-    // Content-Type: text/plain
-    // Content-Length: 11434
-    // x-amz-meta-author: Janet
-    // Expect: 100-continue
-
     S3CanonicalRequest request("PUT",
-                       "https://wolfsuperbuckettest.s3.amazonaws.com",
+                       mirror.mirror.url,
                        path,
                        digest);
     request.hashed_payload = digest;
 
-    CURLHandle uploadrequest(fmt::format("https://wolfsuperbuckettest.s3.amazonaws.com/{}", path));
+    CURLHandle uploadrequest(
+        fmt::format("{}/{}", mirror.mirror.url, path)
+    );
     uploadrequest
-        // .setopt(CURLOPT_UPLOAD, 1L)
-        .setopt(CURLOPT_PUT, 1L)
+        .setopt(CURLOPT_UPLOAD, 1L)
         .setopt(CURLOPT_VERBOSE, 1L)
         .setopt(CURLOPT_INFILESIZE_LARGE, fsize)
         .add_headers(mirror.get_auth_headers(request))
-        // .add_header(fmt::format("Content-Length: {}", fsize))
-        // .add_header("Content-Type: application/octet-stream")
     ;
 
     std::ifstream ufile(file, std::ios::in | std::ios::binary);
