913035b92457c893f4b7ca7e700ed4882f60cc1a
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-12-21 11:45:30 +0100

    fix specifying bucket in URL for S3

--- src/uploader/s3_upload.cpp
+++ src/uploader/s3_upload.cpp
@@ -10,19 +10,19 @@ namespace powerloader
         std::string digest = sha256sum(file);
         std::size_t fsize = fs::file_size(file);
 
-        S3CanonicalRequest request("PUT", mirror.url, path, digest);
+        URLHandler uh(fmt::format("{}/{}", mirror.url, path));
+
+        S3CanonicalRequest request("PUT", uh.url_without_path(), uh.path(), digest);
         request.hashed_payload = digest;
 
         // this is enough to make a file completely public
         // request.headers["x-amz-acl"] = "public-read";
 
-        CURLHandle uploadrequest(fmt::format("{}/{}", mirror.url, path));
+        CURLHandle uploadrequest(uh.url());
 
         std::ifstream ufile(file, std::ios::in | std::ios::binary);
 
-        uploadrequest
-            // .setopt(CURLOPT_VERBOSE, 1L)
-            .setopt(CURLOPT_INFILESIZE_LARGE, fsize)
+        uploadrequest.setopt(CURLOPT_INFILESIZE_LARGE, fsize)
             .add_headers(mirror.get_auth_headers(request))
             .upload(ufile);
 
