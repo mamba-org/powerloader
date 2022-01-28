e8d85355b3f01eb25bed15f0cd32bdf771cae947
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-01-28 15:50:02 +0100

    add barebones pybind11 (#76)

---
+++ src/python/main.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/iostream.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "downloader.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pypowerloader, m)
+{
+    using namespace powerloader;
+
+    m.def("hello_world", []() { std::cout << "Hello world!" << std::endl; });
+}
