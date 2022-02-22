13a75a9abd06c1bb93566c684405da454d47a08c
Johan Mabille <johan.mabille@gmail.com>
2022-02-22 14:17:55 +0100

    Refactored curl handler (#83)

--- include/curl.hpp
+++ include/curl.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef PW_CURL_HPP
+#define PW_CURL_HPP
 
 #include <filesystem>
 #include <functional>
@@ -16,307 +17,105 @@
 
 namespace powerloader
 {
-    class curl_error : public std::runtime_error
-    {
-    public:
-        curl_error(const std::string& what = "download error", bool serious = false)
-            : std::runtime_error(what)
-            , serious(serious)
-        {
-        }
-        bool serious;
-    };
-
-    namespace fs = std::filesystem;
-
     extern "C"
     {
 #include <curl/curl.h>
     }
 
-    template <class T>
-    std::size_t read_callback(char* ptr, std::size_t size, std::size_t nmemb, T* stream);
+    class curl_error : public std::runtime_error
+    {
+    public:
+        curl_error(const std::string& what = "download error", bool serious = false);
+        bool is_serious() const;
 
-    CURL* get_handle();
+    private:
+        bool m_serious;
+    };
 
     struct Response
     {
         std::map<std::string, std::string> header;
         mutable std::stringstream content;
 
-        curl_off_t avg_speed, downloaded_size;
+        curl_off_t avg_speed;
+        curl_off_t downloaded_size;
         long http_status;
         std::string effective_url;
 
-        inline bool ok() const
-        {
-            return http_status / 100 == 2;
-        }
-
-        inline nlohmann::json json() const
-        {
-            try
-            {
-                nlohmann::json j;
-                content >> j;
-                return j;
-            }
-            catch (const nlohmann::detail::parse_error& e)
-            {
-                spdlog::error("Could not parse JSON\n{}", content.str());
-                spdlog::error("Error message: {}", e.what());
-                throw;
-            }
-        }
+        bool ok() const;
+        nlohmann::json json() const;
     };
 
-    template <class T>
-    static std::size_t ostream_callback(char* buffer,
-                                        std::size_t size,
-                                        std::size_t nitems,
-                                        T* stream)
-    {
-        stream->write(buffer, size * nitems);
-        return size * nitems;
-    }
-
-    template <class T>
-    static std::size_t header_map_callback(char* buffer,
-                                           std::size_t size,
-                                           std::size_t nitems,
-                                           T* header_map)
-    {
-        auto kv = parse_header(std::string_view(buffer, size * nitems));
-        if (!kv.first.empty())
-        {
-            (*header_map)[kv.first] = kv.second;
-        }
-        return size * nitems;
-    }
+    // TODO: rename this, try to not expose it
+    CURL* get_handle();
 
     class CURLHandle
     {
     public:
-        CURLHandle()
-            : m_handle(get_handle())
-        {
-            if (m_handle == nullptr)
-            {
-                throw curl_error("Could not initialize handle");
-            }
-            // Set error buffer
-            errorbuffer[0] = '\0';
-            setopt(CURLOPT_ERRORBUFFER, errorbuffer);
-        }
+        using end_callback_type = std::function<CbReturnCode(const Response&)>;
+        CURLHandle();
+        CURLHandle(const std::string& url);
+        ~CURLHandle();
 
-        CURLHandle(const std::string& url)
-            : CURLHandle()
-        {
-            this->url(url);
-        }
+        CURLHandle& url(const std::string& url);
+        CURLHandle& accept_encoding();
+        CURLHandle& user_agent(const std::string& user_agent);
 
-        CURLHandle& url(const std::string& url)
-        {
-            setopt(CURLOPT_URL, url.c_str());
-            return *this;
-        }
-
-        CURLHandle& accept_encoding()
-        {
-            setopt(CURLOPT_ACCEPT_ENCODING, nullptr);
-            return *this;
-        }
-
-        CURLHandle& user_agent(const std::string& user_agent)
-        {
-            add_header(fmt::format("User-Agent: {} {}", user_agent, curl_version()));
-            return *this;
-        }
-
-        // CURLHandle& exists_only(bool use_get = false)
-        // {
-        //     setopt(CURLOPT_FAILONERROR, 1L);
-        //     if (use_get)
-        //         setopt(CURLOPT_NOBODY, 0L);
-        //     else
-        //         setopt(CURLOPT_NOBODY, 1L);
-
-        //     return *this;
-        // }
-
-        inline Response perform()
-        {
-            set_default_callbacks();
-            CURLcode curl_result = curl_easy_perform(handle());
-            if (curl_result != CURLE_OK)
-            {
-                throw curl_error(
-                    fmt::format("{} [{}]", curl_easy_strerror(curl_result), errorbuffer));
-            }
-            finalize_transfer(*response);
-            return std::move(*response.release());
-        }
-
-        inline CURL* handle()
-        {
-            if (p_headers)
-                setopt(CURLOPT_HTTPHEADER, p_headers);
-            return m_handle;
-        }
-
-        inline void finalize_transfer()
-        {
-            if (response)
-                finalize_transfer(*response);
-        }
+        Response perform();
+        void finalize_transfer();
+        // TODO: should be private?
+        void finalize_transfer(Response& response);
 
         template <class T>
-        inline cpp::result<T, CURLcode> getinfo(CURLINFO option)
-        {
-            T val;
-            CURLcode result = curl_easy_getinfo(m_handle, option, &val);
-            if (result != CURLE_OK)
-                return cpp::fail(result);
-            return val;
-        }
+        cpp::result<T, CURLcode> getinfo(CURLINFO option);
+
+        // TODO: why do we need to expose these three methods
+        CURL* handle();
+        operator CURL*();
+        CURL* ptr() const;
+
+        CURLHandle& add_header(const std::string& header);
+        CURLHandle& add_headers(const std::vector<std::string>& headers);
+        CURLHandle& reset_headers();
 
         template <class T>
-        inline void finalize_transfer(T& response)
-        {
-            auto cres = curl_easy_getinfo(m_handle, CURLINFO_SPEED_DOWNLOAD_T, &response.avg_speed);
-            if (cres != CURLE_OK)
-            {
-                response.avg_speed = 0;
-            }
+        CURLHandle& setopt(CURLoption opt, const T& val);
 
-            char* tmp_effective_url;
-            // TODO error handling?!
-            curl_easy_getinfo(m_handle, CURLINFO_RESPONSE_CODE, &response.http_status);
-            curl_easy_getinfo(m_handle, CURLINFO_EFFECTIVE_URL, &tmp_effective_url);
-            curl_easy_getinfo(m_handle, CURLINFO_SIZE_DOWNLOAD_T, &response.downloaded_size);
+        void set_default_callbacks();
+        CURLHandle& set_end_callback(const end_callback_type& func);
 
-            response.effective_url = tmp_effective_url;
+        CURLHandle& upload(std::ifstream& stream);
+        CURLHandle& upload(std::istringstream& stream);
 
-            if (!response.ok())
-            {
-                spdlog::error("Received {}: {}", response.http_status, response.content.str());
-            }
-            if (end_callback)
-            {
-                end_callback(response);
-            }
-        }
-
-        inline operator CURL*()
-        {
-            return handle();
-        }
-
-        inline CURLHandle& add_header(const std::string& header)
-        {
-            p_headers = curl_slist_append(p_headers, header.c_str());
-            if (!p_headers)
-            {
-                throw std::bad_alloc();
-            }
-            return *this;
-        }
-
-        inline CURLHandle& add_headers(const std::vector<std::string>& headers)
-        {
-            for (auto& h : headers)
-            {
-                add_header(h);
-            }
-            return *this;
-        }
-
-        inline CURLHandle& reset_headers()
-        {
-            curl_slist_free_all(p_headers);
-            p_headers = nullptr;
-            return *this;
-        }
-
-        template <class T>
-        inline CURLHandle& setopt(CURLoption opt, const T& val)
-        {
-            CURLcode ok;
-            if constexpr (std::is_same<T, std::string>())
-            {
-                ok = curl_easy_setopt(m_handle, opt, val.c_str());
-            }
-            else
-            {
-                ok = curl_easy_setopt(m_handle, opt, val);
-            }
-            if (ok != CURLE_OK)
-            {
-                throw curl_error(
-                    fmt::format("curl: curl_easy_setopt failed {}", curl_easy_strerror(ok)));
-            }
-            return *this;
-        }
-
-        ~CURLHandle()
-        {
-            if (m_handle)
-            {
-                curl_easy_cleanup(m_handle);
-            }
-            if (p_headers)
-            {
-                curl_slist_free_all(p_headers);
-            }
-        }
-
-        inline CURL* ptr() const
-        {
-            return m_handle;
-        }
-
-        inline void set_default_callbacks()
-        {
-            response.reset(new Response);
-            // check if there is something set already for these values
-            setopt(CURLOPT_HEADERFUNCTION, header_map_callback<std::map<std::string, std::string>>);
-            setopt(CURLOPT_HEADERDATA, &response->header);
-
-            setopt(CURLOPT_WRITEFUNCTION, ostream_callback<std::stringstream>);
-            setopt(CURLOPT_WRITEDATA, &response->content);
-        }
-
-        template <class S>
-        inline CURLHandle& upload(S& stream)
-        {
-            stream.seekg(0, stream.end);
-            curl_off_t fsize = stream.tellg();
-            stream.seekg(0, stream.beg);
-
-            if (fsize != -1)
-            {
-                setopt(CURLOPT_INFILESIZE_LARGE, fsize);
-            }
-
-            setopt(CURLOPT_UPLOAD, 1L);
-            setopt(CURLOPT_READFUNCTION, read_callback<S>);
-            setopt(CURLOPT_READDATA, &stream);
-            return *this;
-        }
-
-        inline CURLHandle& set_end_callback(
-            const std::function<CbReturnCode(const Response&)>& func)
-        {
-            end_callback = func;
-            return *this;
-        }
-
+    private:
         CURL* m_handle;
         curl_slist* p_headers = nullptr;
         char errorbuffer[CURL_ERROR_SIZE];
 
         std::unique_ptr<Response> response;
-        std::function<CbReturnCode(const Response&)> end_callback;
+        end_callback_type end_callback;
     };
 
+    // TODO: restrict the possible implementations in the cpp file
+    template <class T>
+    inline CURLHandle& CURLHandle::setopt(CURLoption opt, const T& val)
+    {
+        CURLcode ok;
+        if constexpr (std::is_same<T, std::string>())
+        {
+            ok = curl_easy_setopt(m_handle, opt, val.c_str());
+        }
+        else
+        {
+            ok = curl_easy_setopt(m_handle, opt, val);
+        }
+        if (ok != CURLE_OK)
+        {
+            throw curl_error(
+                fmt::format("curl: curl_easy_setopt failed {}", curl_easy_strerror(ok)));
+        }
+        return *this;
+    }
 }
+
+#endif
