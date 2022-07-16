d378cc14572a7b1d4a3acf7838810e8efcf4b1f3
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-07-15 23:34:09 -0500

    working and fast version

--- src/cli/jlap.cpp
+++ src/cli/jlap.cpp
@@ -2,6 +2,7 @@
 #include <powerloader/context.hpp>
 #include <powerloader/fileio.hpp>
 #include <openssl/evp.h>
+#include <ctime>
 
 extern "C"
 {
@@ -56,6 +57,7 @@ apply_jlap()
     std::cout << blake2sum("repodata.jlap") << std::endl;
     std::vector<Patch> patches;
     std::unique_ptr<Patch> cur_patch;
+
     while (std::getline(jlap_file, line))
     {
         if (line.size() && line[0] != '{')
@@ -68,9 +70,16 @@ apply_jlap()
         }
         else
         {
-            auto j = nlohmann::json::parse(line);
-            // std::cout << j.dump(4) << std::endl;
-            cur_patch->patches.emplace_back(std::move(j));
+            try
+            {
+                auto j = nlohmann::json::parse(line);
+                // std::cout << j.dump(4) << std::endl;
+                cur_patch->patches.emplace_back(std::move(j));
+            }
+            catch(...)
+            {
+                std::cout << "Could not parse patch " << line << std::endl;
+            }
         }
     }
 
@@ -80,6 +89,7 @@ apply_jlap()
     std::string repo_bsum = blake2sum("repodata.json");
 
     std::size_t i = 0;
+
     for (auto& p : patches)
     {
         std::cout << "Found patches # " << p.patches.size() << std::endl;
@@ -89,16 +99,31 @@ apply_jlap()
             {
                 if (pf["from"] == repo_bsum)
                 {
-                    std::cout << "Applying patch " << i++ << " from " << repo_bsum << " to "
-                              << pf["to"] << std::endl;
+                    auto t0 = std::chrono::high_resolution_clock::now();
+                    std::cout << "Applying patch " << i++ << " from " << repo_bsum.substr(0, 8) << "…2 to "
+                              << pf["to"].get<std::string>().substr(0, 8) << "… ";
                     repo_bsum = pf["to"];
-                    jrdata = jrdata.patch(pf["patch"]);
+                    // nlohmann::inplace_patch(jrdata, pf["patch"]);
+                    jrdata.patch(pf["patch"]);
+                    auto t1 = std::chrono::high_resolution_clock::now();
+                    std::cout << "took " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count() <<" ns." << std::endl;
                 }
             }
         }
     }
-    std::ofstream rpatched("final_repodata.json");
-    rpatched << jrdata.dump(2) << "\n";
+    {
+        std::ofstream rpatched("final_repodata.json");
+        rpatched << jrdata.dump(2) << "\n";
+    }
+
+    if (blake2sum("final_repodata.json") == patches.back().patches.back()["latest"])
+    {
+        std::cout << "All successful." << std::endl;
+    }
+    else
+    {
+        std::cout << "Error: " << blake2sum("final_repodata.json") << " not matching " << patches.back().patches.back()["latest"] << std::endl;
+    }
 }
 
 
@@ -119,7 +144,8 @@ main()
     for (auto& [k, v] : response.headers)
         std::cout << k << " .. " << v << std::endl;
 
-    outfile.write(response.content.value());
+    if (response.ok())
+        outfile.write(response.content.value());
     apply_jlap();
     return 0;
 }
