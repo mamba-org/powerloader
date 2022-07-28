36a2f92d2148b57e33b9b5f4c362c91d73c90d18
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2022-07-28 15:15:27 +0200

    Removed declarations of inexisting zchunk functions

--- src/zck.cpp
+++ src/zck.cpp
@@ -18,16 +18,9 @@ namespace powerloader
 
     bool zck_valid_header_base(const Checksum& chksum, std::ptrdiff_t zck_header_size, int fd);
 
-    // bool zck_clear_header(Target& target);
-
     std::vector<fs::path> get_recursive_files(const fs::path& dir, const std::string& suffix);
 
-    // TODO replace...
-    // int lr_copy_content(int source, int dest);
-
     bool find_local_zck_header(Target& target);
-
-    // bool prep_zck_header(Target& target);
 
     bool find_local_zck_chunks(Target& target);
 
