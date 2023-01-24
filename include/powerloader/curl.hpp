a45c378395223276c0aa260762bb4c6e13b006e9
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-01-24 09:20:38 +0100

    add powerloader init function

--- include/powerloader/curl.hpp
+++ include/powerloader/curl.hpp
@@ -25,6 +25,30 @@ namespace powerloader
 
 #include <curl/curl.h>
 
+    enum class ssl_backend_t
+    {
+        none = CURLSSLBACKEND_NONE,
+        openssl = CURLSSLBACKEND_OPENSSL,
+        gnutls = CURLSSLBACKEND_GNUTLS,
+        nss = CURLSSLBACKEND_NSS,
+        gskit = CURLSSLBACKEND_GSKIT,
+        // polarssl = CURLSSLBACKEND_POLARSSL /* deprecated by curl */,
+        wolfssl = CURLSSLBACKEND_WOLFSSL,
+        schannel = CURLSSLBACKEND_SCHANNEL,
+        securetransport = CURLSSLBACKEND_SECURETRANSPORT,
+        // axtls = CURLSSLBACKEND_AXTLS, /* deprecated by curl */
+        mbedtls = CURLSSLBACKEND_MBEDTLS,
+        // mesalink = CURLSSLBACKEND_MESALINK, /* deprecated by curl */
+        bearssl = CURLSSLBACKEND_BEARSSL,
+        rustls = CURLSSLBACKEND_RUSTLS,
+    };
+
+    /*
+    To set ssl backends, use the `init` function before using any other function from the
+    powerloader library.
+    */
+    bool init(const std::optional<ssl_backend_t>& ssl_backend = {});
+
     class POWERLOADER_API curl_error : public std::runtime_error
     {
     public:
